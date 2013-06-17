a1267e4c7132a78e2005f668f09b690a6c6fd4d9
Jan Machacek <jan.machacek@gmail.com>
2013-06-17 08:35:03 +0100

    SPC

---
+++ cli/src/main/scala/com/typesafe/activator/extras/tutorial/TutorialCli.scala
@@ -0,0 +1,7 @@
+package com.typesafe.activator.extras.tutorial
+
+object TutorialCli extends App with Tutorial {
+
+
+
+}
--- core/src/main/scala/com/typesafe/activator/extras/tutorial/Tutorial.scala
+++ core/src/main/scala/com/typesafe/activator/extras/tutorial/Tutorial.scala
@@ -1,8 +1,19 @@
 package com.typesafe.activator.extras.tutorial
 
+import scala.tools.nsc.Settings
+import scala.tools.nsc.reporters.ConsoleReporter
+
 trait Tutorial {
   import scala.tools.nsc.Global
 
+  val settings = new Settings()
+  val reporter = new ConsoleReporter(settings)
+  val compiler = new Global(settings, reporter)
 
+  val src = "package foo; case class X();"
+  val x = compiler.newUnitParser(src)
+  val y = x.parse()
+
+  println(y)
 
 }
